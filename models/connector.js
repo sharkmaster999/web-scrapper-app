53c3944a32fb9a3475299669449d41eba442dd60
Remulta Remuelito <remulta.remuelito@ibm.com>
2024-10-08 11:25:29 +0800

    Setup sequelize

---
+++ models/connector.js
@@ -0,0 +1,32 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize({
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+});
+
+async function openConnection() {
+    try {
+        await sequelize.authenticate();
+    } catch (error) {
+        console.error('Unable to connect to database: ', error);
+    }
+}
+
+async function closeConnection() {
+    try {
+        await sequelize.close();
+    } catch (error) {
+        console.error('Unable to connect to database: ', error);
+    }
+}
+
+module.exports = {
+    openConnection,
+    closeConnection,
+    sequelize
+};
